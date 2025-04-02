18a5d00ed4b5ba94b64a2467eed58d1f2266d7f8
changhyeon <changhyeon.h@kakao.com>
2025-04-02 21:59:58 +0900

    init: init commit

---
+++ src/main/java/org/changhyeon/imagemerger/ImageMergerApplication.java
@@ -0,0 +1,13 @@
+package org.changhyeon.imagemerger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageMergerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageMergerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/changhyeon/imagemerger/ImageMergerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.changhyeon.imagemerger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageMergerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
